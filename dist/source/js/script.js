fe13e4a1bfc4722fc5d4045e426f3b3d84eaf5fd
Hatvis7 <shamil.gasanov.2004@bk.ru>
2021-06-16 08:57:39 +0300

    Добавлена и адаптирована шапка
    Создана шапка и создан адаптив под неё, так же было добавлено бургер меню.

--- dist/source/js/script.js
+++ dist/source/js/script.js
@@ -4,13 +4,4 @@
     burger.addEventListener('click', () => {
         burger.classList.toggle('burger__active');
     });
-
-    const accordeon = document.querySelectorAll('.service__accordeon-item-icon');
-
-    for(let elem of accordeon){
-    elem.addEventListener('click', () => {
-    elem.classList.toggle('accordeon__active');
-    });
-    }
-
 }());
