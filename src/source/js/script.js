6843e70ca50f3c2c8e01daace129db4f1d7a0840
Hatvis7 <shamil.gasanov.2004@bk.ru>
2021-06-16 09:19:52 +0300

    Revert "Добавлена и адаптирована шапка"
    This reverts commit fe13e4a1bfc4722fc5d4045e426f3b3d84eaf5fd.

--- src/source/js/script.js
+++ src/source/js/script.js
@@ -4,4 +4,13 @@
     burger.addEventListener('click', () => {
         burger.classList.toggle('burger__active');
     });
+
+    const accordeon = document.querySelectorAll('.service__accordeon-item-icon');
+
+    for(let elem of accordeon){
+    elem.addEventListener('click', () => {
+    elem.classList.toggle('accordeon__active');
+    });
+    }
+
 }());
